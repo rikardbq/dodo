ca475344c3a14214c890356080960f696e1308e9
rikardbq <j10rikardb@gmail.com>
2026-06-23 13:46:04 +0200

    test some stuff to see how the tasks should be read from disk

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use std::{fs::File, io::Read};
+
+pub fn get_file_content(path: &str) -> Vec<u8> {
+    let mut file_buf = Vec::<u8>::new();
+    let _ = (match File::open(path) {
+        Ok(f) => f,
+        Err(err) => {
+            panic!("{:?}", err)
+        }
+    })
+    .read_to_end(&mut file_buf);
+
+    file_buf
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use dodo::get_file_content;
+
 fn main() {
+    let file_string = String::from_utf8_lossy(&get_file_content("example"))
+        .replace("\r\n", " ");
+    let file_string_split = file_string
+        .split("#####")
+        .map(|x| x.trim())
+        .collect::<Vec<&str>>();
+    println!("BEFORE: {file_string:?}");
+    println!("AFTER: {file_string_split:?}");
     println!("todo: create dodo, make no mistakes!");
 }
